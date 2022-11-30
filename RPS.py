92c09d70ce14b1992a403c76e24cf65e71aa67f9
Brian Bui <brianbui1231@yahoo.com>
2022-11-30 02:06:09 -0600

    Changed the code

--- RPS.py
+++ RPS.py
@@ -1,19 +1,28 @@
 import random
+yourChoice = input("Enter your choice: Rock, Paper, or Scissor:\n")
+enemy = ['rock', 'paper', 'scissor']
+computer1 = random.choice(enemy) 
+def winChoice(player, computer):
+    player.lower()
+    computer.lower()
+    if player == computer:
+        print("You Tied!\n")
+    else:
+        if player == "rock" and computer == "scissor":
+            print("You Won!\n")
+        if player == "scissor" and computer == "rock":
+            print("You Lost!\n")
+        if player == "paper" and computer == "rock":
+            print("You Won!\n")
+        if player == "paper" and computer == "scissor":
+            print("You Lost!\n")
+        if player == "scissor" and computer == "paper":
+            print("You Won!\n")
+        if player == "rock" and computer == "paper":
+            print("You Lost!\n")
+winChoice(yourChoice, computer1)
+c = input("Enter anything to continue, else enter q to quit:\n")
 
-
-def play():
-    user = input("'r' for rock, 'p' for paper, 's' for scissors")
-    computer = random.choice(['r', 'p', 's'])
-
-    if user == computer:
-        return 'tie'
-    if is_win(user, computer):
-        return 'you won!'
-    return 'you lost!'
-
-
-def is_win(player, opponent):
-    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
-            or (player == 'p' and opponent == 'r'):
-        return True
-print(play())
+while c != 'q' and yourChoice in enemy:
+    yourChoice = input("Enter your choice: Rock, Paper, or Scissor:\n")
+    winChoice(yourChoice, computer1)
