c269a6a470d81cd2e6ef88dd9419b5cabb03d199
brianbui1231 <108392130+brianbui1231@users.noreply.github.com>
2022-11-30 22:56:48 -0600

    Update DiceRoller.py

--- DiceRoller.py
+++ DiceRoller.py
@@ -1,1 +1,28 @@
+import random
+import time
+dices = int(input("Enter amount of dices:\n"))
+numDice = int(input("Enter the max amount number a dice should have:\n"))
+def rollDice(x, y):
+    c = input("Enter anything to roll or enter q to quit:\n")
+    dice1 = 1
+    total = 0
+    while c != 'q':
+        if y <= 9:
+            for i in range(x):
+                print("Rolling...")
+                time.sleep(1)
+                print("Rolling...")
+                time.sleep(.5)
+                randomNum = random.randint(0, y)
+                print(f'Dice {dice1}: {randomNum}')
+                total += randomNum
+                dice1 += 1
+        else:
+            print("You entered a number that isn't possible for a die:\n")
+            y = int(input("Enter a new number:\n"))
+        c = input("Enter anything to roll or enter q to quit:\n")
+    print("Your total is: ", total)
+    return 
+rollDice(dices, numDice)
+    
 
