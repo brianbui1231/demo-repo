9052f14336090d25ed5192516c7e41457b61768e
Brian Bui <brianbui1231@yahoo.com>
2023-02-28 21:36:21 -0600

    Remove

--- PasswordNumberGen.py
+++
@@ -1,5 +0,0 @@
-import random
-
-amountNum = int(input("Enter the amount of numbers for your pin that you want to generate:\n"))
-for num in range(amountNum):
-    print(random.randint(0, 9), end ='')
